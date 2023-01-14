2416eb43ad00e29570d51ddf393a7a94e56bad11
Aditya Tyagi <adityatyagiav1@gmail.com>
2023-01-14 21:55:36 +0530

    3.3 completed

--- index.js
+++ index.js
@@ -27,9 +27,7 @@ let persons=[
 app.get('/',(req,res)=>{
     res.send("<h1>app.get working</h1>")
 })
-app.get('/api/persons',(req,res)=>{
-    res.json(persons)
-})
+
 app.get('/info',(req,res)=>{
     const d = new Date()
     console.log(d)
@@ -38,8 +36,48 @@ app.get('/info',(req,res)=>{
                 <p>${d}</p>
                 </div>`)
 })
+app.get('/api/persons/:id',(req,res)=>{
+    const id=Number(req.params.id)
+    console.log(id)
+    const person=persons.find(person=>person.id===id)
+    console.log(person)
+    if(person){
+       res.json(person) 
+    }
+    else{
+        res.status(400).end()
+    }
+})
 
+app.delete('/api/persons/:id',(req,res)=>{
+    const id=Number(req.params.id)
+    const person=persons.filter(person=>person.id!==id)
+    res.send('Delete request to homepage')
+    return res.status(204).end()
+})
+app.get('/api/persons',(req,res)=>{
+    res.json(persons)
+})
 
+app.post('/api/persons',(req,res)=>{
+    const body=req.body
+    const generateId=()=>{
+        return Math.floor((Math.random()*100)+person.length)
+    }
+    if(!body.name){
+        return res.status(400).json({
+            error: 'content missing'
+    })
+    }
+    const person ={
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+    persons =persons.concat(person)
+    console.log(person)
+    res.json(person)
+})
 
 const PORT =3001
 app.listen(PORT)
