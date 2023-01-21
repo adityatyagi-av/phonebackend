6a58f182c211d6e7766c51a9f7e14d1a796e1875
Aditya Tyagi <adityatyagiav1@gmail.com>
2023-01-22 03:11:12 +0530

    separate models created for db

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
 const express = require('express') 
 const app=express()
 
-let persons=[
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+require('dotenv').config()
+const Person = require('./models/phone')
+
+app.use(express.json())
 
 app.get('/',(req,res)=>{
     res.send("<h1>app.get working</h1>")
@@ -37,9 +19,9 @@ app.get('/info',(req,res)=>{
                 </div>`)
 })
 app.get('/api/persons/:id',(req,res)=>{
-    const id=Number(req.params.id)
-    console.log(id)
-    const person=persons.find(person=>person.id===id)
+    const id=req.params.id
+    console.log(typeof(id))
+    const person=Person.find(person=>person.id===id)
     console.log(person)
     if(person){
        res.json(person) 
@@ -50,35 +32,41 @@ app.get('/api/persons/:id',(req,res)=>{
 })
 
 app.delete('/api/persons/:id',(req,res)=>{
-    const id=Number(req.params.id)
-    const person=persons.filter(person=>person.id!==id)
+    const person=Person.filter(person=>person.id!==id)
     res.send('Delete request to homepage')
     return res.status(204).end()
 })
 app.get('/api/persons',(req,res)=>{
-    res.json(persons)
+    Person.find({}).then(person=>{
+        res.json(person)
+    })
+    
 })
 
 app.post('/api/persons',(req,res)=>{
     const body=req.body
-    const generateId=()=>{
-        return Math.floor((Math.random()*100)+person.length)
-    }
-    if(!body.name){
+    if(body.name===undefined){
         return res.status(400).json({
             error: 'content missing'
     })
     }
-    const person ={
+
+    const person =new Person({
         name: body.name,
         number: body.number,
-        id: generateId()
-    }
-    persons =persons.concat(person)
-    console.log(person)
-    res.json(person)
+    })
+    person.save().then(result=>{
+        res.json(result)
+    })
+    
 })
 
-const PORT =3001
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  app.use(unknownEndpoint)
+  
+const PORT =process.env.PORT||3001
 app.listen(PORT)
     console.log(`Server running on port ${PORT}`)
